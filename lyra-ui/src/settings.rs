95a6caaab90e2b0463d39d1a3a115b2ee7af88c1
Dylan Fontana <dyfontana@gmail.com>
2024-02-23 20:10:33 -0500

    Working on settings table for webq

--- lyra-ui/src/settings.rs
+++ lyra-ui/src/settings.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use egui::ViewportId;
+use egui_extras::{Column, TableBuilder};
 use parking_lot::RwLock;
 
 const LYRA_SETTINGS: &str = "Lyra Settings";
@@ -9,6 +10,12 @@ pub struct LyraSettings {
   pub id: ViewportId,
   pub title: String,
   pub visible: Arc<RwLock<bool>>,
+  // AppData
+  window_x: String,
+  window_y: String,
+  webq_label: String,
+  webq_template: String,
+  webq_image: String,
 }
 
 impl Default for LyraSettings {
@@ -17,12 +24,17 @@ impl Default for LyraSettings {
       id: ViewportId::from_hash_of(LYRA_SETTINGS),
       title: LYRA_SETTINGS.into(),
       visible: Arc::new(RwLock::new(false)),
+      window_x: String::new(),
+      window_y: String::new(),
+      webq_label: String::new(),
+      webq_template: String::new(),
+      webq_image: String::new(),
     }
   }
 }
 
 impl LyraSettings {
-  pub fn update(&self, ctx: &egui::Context) {
+  pub fn update(&mut self, ctx: &egui::Context) {
     if !*self.visible.read() {
       return;
     }
@@ -32,13 +44,62 @@ impl LyraSettings {
         .with_title(&self.title)
         .with_inner_size([600.0, 500.0]),
       |ctx, _| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-          ui.label("Hello from immediate viewport");
-        });
-
         if ctx.input(|i| i.viewport().close_requested()) {
           *self.visible.write() = false;
         }
+        egui::CentralPanel::default().show(ctx, |ui| {
+          ui.horizontal_top(|ui| {
+            ui.label("Window Placement");
+            ui.text_edit_singleline(&mut self.window_x);
+            ui.text_edit_singleline(&mut self.window_y);
+          });
+          ui.separator();
+          ui.horizontal(|ui| {
+            ui.text_edit_singleline(&mut self.webq_label);
+            ui.text_edit_singleline(&mut self.webq_template);
+            let img = self.webq_image.clone();
+            ui.text_edit_singleline(&mut self.webq_image);
+            if !img.is_empty() {
+              ui.image(img);
+            }
+          });
+          ui.separator();
+          ui.horizontal(|ui| {
+            TableBuilder::new(ui)
+              .striped(true)
+              .resizable(false)
+              .vscroll(true)
+              .auto_shrink(true)
+              .column(Column::auto())
+              .column(Column::auto())
+              .column(Column::auto())
+              .column(Column::auto())
+              .header(32f32, |mut r| {
+                r.col(|ui| {
+                  ui.label("Label");
+                });
+                r.col(|ui| {
+                  ui.label("Template");
+                });
+                r.col(|ui| {
+                  ui.label("Image");
+                });
+                r.col(|ui| {
+                  ui.label("Delete");
+                });
+              })
+              .body(|mut body| {
+                let rows = 100;
+                body.rows(32f32, rows, |mut row| {
+                  // TODO: Render row of data
+                });
+              });
+          });
+          ui.separator();
+          if ui.button("Save").clicked() {
+            // TODO: Save form
+          }
+        });
       },
     );
   }
