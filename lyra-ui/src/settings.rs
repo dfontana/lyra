3563795ea7e9742b2437ecd03889421fb45896ff
Dylan Fontana <dyfontana@gmail.com>
2024-04-18 19:45:49 -0400

    more field tweaks

--- lyra-ui/src/settings.rs
+++ lyra-ui/src/settings.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use egui::{Color32, Layout, Stroke, TextEdit, ViewportId, Widget};
 use egui_extras::{Column, TableBuilder};
 use parking_lot::RwLock;
-use tracing::warn;
+use tracing::{info, warn};
 
 use crate::config::{Config, Placement};
 
@@ -15,11 +15,11 @@ pub struct LyraSettings {
   pub visible: Arc<RwLock<bool>>,
   config: Arc<Config>,
   // AppData
-  window_x: String,
-  window_y: String,
-  webq_label: String,
-  webq_template: String,
-  webq_image: String,
+  window_x: FieldData<f32>,
+  window_y: FieldData<f32>,
+  webq_label: FieldData<String>,
+  webq_template: FieldData<String>,
+  webq_image: FieldData<String>,
 }
 
 impl LyraSettings {
@@ -30,8 +30,8 @@ impl LyraSettings {
       let cfg = config.get();
       match cfg.styles.window_placement {
         Placement::XY(x, y) => {
-          window_x = x.to_string();
-          window_y = y.to_string();
+          window_x = x;
+          window_y = y;
         }
       }
     }
@@ -39,19 +39,19 @@ impl LyraSettings {
       id: ViewportId::from_hash_of(LYRA_SETTINGS),
       title: LYRA_SETTINGS.into(),
       visible: Arc::new(RwLock::new(false)),
-      window_x,
-      window_y,
-      webq_label: String::new(),
-      webq_template: String::new(),
-      webq_image: String::new(),
+      window_x: FieldData::new(window_x),
+      window_y: FieldData::new(window_y),
+      webq_label: FieldData::new(String::new()),
+      webq_template: FieldData::new(String::new()),
+      webq_image: FieldData::new(String::new()),
       config,
     }
   }
 }
 
 impl LyraSettings {
-  fn validate_xy(v: f32) -> Result<(), String> {
-    if v < 0.0 {
+  fn validate_xy(v: &f32) -> Result<(), String> {
+    if *v < 0.0 {
       return Err("Must be larger than 0".into());
     }
     Ok(())
@@ -74,26 +74,24 @@ impl LyraSettings {
           ui.horizontal_top(|ui| {
             ui.label("Window Placement");
             ui.add(
-              // TODO: Store a ref to this input and extract value on save
-              //       Or is there a better way to gather the form :thinking:
-              Input::new("X:", &mut self.window_x)
-                .f32(&LyraSettings::validate_xy)
+              Input::f32("X:", &mut self.window_x)
+                .validated(&LyraSettings::validate_xy)
                 .desired_width(35.0),
             );
             ui.add(
-              Input::new("Y:", &mut self.window_y)
-                .f32(&LyraSettings::validate_xy)
+              Input::f32("Y:", &mut self.window_y)
+                .validated(&LyraSettings::validate_xy)
                 .desired_width(35.0),
             );
           });
           ui.separator();
           ui.label("Default Search");
           ui.vertical(|ui| {
-            ui.add(Input::new("Label:", &mut self.webq_label).desired_width(200.0));
-            ui.add(Input::new("Template:", &mut self.webq_template).desired_width(400.0));
+            ui.add(Input::str("Label:", &mut self.webq_label).desired_width(200.0));
+            ui.add(Input::str("Template:", &mut self.webq_template).desired_width(400.0));
             ui.horizontal(|ui| {
-              let img = self.webq_image.clone();
-              ui.add(Input::new("Image:", &mut self.webq_image).desired_width(400.0));
+              let img = self.webq_image.value.clone();
+              ui.add(Input::str("Image:", &mut self.webq_image).desired_width(400.0));
               if !img.is_empty() {
                 ui.image(img);
               }
@@ -136,8 +134,8 @@ impl LyraSettings {
             self.config.update(|mut inner| {
               inner.styles.window_placement = Placement::XY(
                 // TODO: Verify user has input a valid value; you might want to make a widget
-                self.window_x.parse::<f32>().unwrap(),
-                self.window_y.parse::<f32>().unwrap(),
+                self.window_x.value,
+                self.window_y.value,
               );
             });
             if let Err(err) = self.config.persist() {
@@ -150,34 +148,53 @@ impl LyraSettings {
   }
 }
 
+struct FieldData<T> {
+  buffer: String,
+  // TODO: Perhaps Value,Error should be a single Result instead
+  value: T,
+  error: Option<String>,
+}
+
+impl<T: ToString> FieldData<T> {
+  pub fn new(value: T) -> FieldData<T> {
+    let buffer = value.to_string();
+    FieldData {
+      value,
+      buffer,
+      error: None,
+    }
+  }
+}
+
 struct Input<'a, T> {
   label: &'a str,
-  value: &'a mut String,
-  validator: Box<dyn Fn(&String) -> Result<T, String>>,
+  field: &'a mut FieldData<T>,
+  parser: Box<dyn Fn(&String) -> Result<T, String>>,
+  validator: Box<dyn Fn(&T) -> Result<(), String>>,
   desired_width: Option<f32>,
 }
 
 impl<'a> Input<'a, String> {
-  pub fn new(label: &'a str, value: &'a mut String) -> Input<'a, String> {
+  pub fn str(label: &'a str, field: &'a mut FieldData<String>) -> Input<'a, String> {
     Input {
       label,
-      value,
+      field,
       desired_width: None,
-      validator: Box::new(|v| Ok(v.to_owned())),
+      parser: Box::new(|v| Ok(v.to_owned())),
+      validator: Box::new(|_| Ok(())),
     }
   }
 
-  pub fn f32(self, validator: impl Fn(f32) -> Result<(), String> + 'static) -> Input<'a, f32> {
-    let wrapped = Box::new(move |v: &String| {
-      v.parse::<f32>()
-        .map_err(|_| format!("{} is not a number", v))
-        .and_then(|v| (validator)(v).map(|_| v))
-    });
+  pub fn f32(label: &'a str, field: &'a mut FieldData<f32>) -> Input<'a, f32> {
     Input {
-      validator: wrapped,
-      label: self.label,
-      value: self.value,
-      desired_width: self.desired_width,
+      label,
+      field,
+      desired_width: None,
+      parser: Box::new(|v| {
+        v.parse::<f32>()
+          .map_err(|_| format!("{} is not a number", v))
+      }),
+      validator: Box::new(|_| Ok(())),
     }
   }
 }
@@ -189,12 +206,24 @@ impl<'a, T> Input<'a, T> {
     self
   }
 
-  fn validate(&self) -> Result<T, String> {
-    (self.validator)(&self.value)
+  #[inline]
+  fn validated(mut self, validator: impl Fn(&T) -> Result<(), String> + 'static) -> Self {
+    self.validator = Box::new(validator);
+    self
   }
 
-  pub fn value(&self) -> Option<T> {
-    self.validate().ok()
+  fn parse_and_validate(self) {
+    match (self.parser)(&self.field.buffer) {
+      Ok(v) => self.field.value = v,
+      Err(err) => {
+        self.field.error = Some(err);
+        return;
+      }
+    };
+    match (self.validator)(&self.field.value) {
+      Ok(()) => self.field.error = None,
+      Err(err) => self.field.error = Some(err),
+    };
   }
 }
 
@@ -205,19 +234,21 @@ impl<'a, T> Widget for Input<'a, T> {
       Layout::left_to_right(egui::Align::Min),
       |ui| {
         ui.label(self.label);
-        let err = self.validate().err();
-        if err.is_some() {
+        if self.field.error.is_some() {
           let invalid = Stroke::new(1.0, Color32::RED);
           ui.style_mut().visuals.widgets.inactive.bg_stroke = invalid;
           ui.style_mut().visuals.widgets.hovered.bg_stroke = invalid;
           ui.style_mut().visuals.selection.stroke = invalid;
         };
-        let edit = ui.add(
-          TextEdit::singleline(self.value)
+        let mut edit = ui.add(
+          TextEdit::singleline(&mut self.field.buffer)
             .desired_width(self.desired_width.unwrap_or(f32::INFINITY)),
         );
-        if let Some(err) = err {
-          edit.on_hover_text(err);
+        if let Some(err) = &self.field.error {
+          edit = edit.on_hover_text(err);
+        }
+        if edit.changed() {
+          self.parse_and_validate();
         }
       },
     )
